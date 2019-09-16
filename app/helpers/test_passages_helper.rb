f04ab48be5dad6b2e750b74fcd7f59f681a82a0c
Aliya <sophiliya@gmail.com>
2019-09-16 21:51:41 +0600

    Add helper to test_passages/result

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,15 @@
 module TestPassagesHelper
+  def show_score
+    content_tag :h3, @test_passage.correct_questions, class: "result-#{result_type}"
+  end
+
+  def show_message
+    content_tag :p, "Тест #{result_type == 'success' ? 'успешно пройден' : 'не пройден'}"
+  end
+
+  private
+
+  def result_type
+    @result >= 0.85 ? 'success' : 'fail'
+  end
 end
